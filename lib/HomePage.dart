cee9d93cb24682423155bd698a5f8f7eb458a9ba
QaysAlsary <qays1337@gmail.com>
2022-08-17 19:12:25 +0300

    commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,238 @@
+import 'dart:async';
+
+import 'package:flappy_bird_game/barriers.dart';
+import 'package:flappy_bird_game/bird.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static double birdYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = birdYaxis;
+  bool gameHasStarted = false;
+  double barrierXtwo = barrierXone + 1.5;
+  static double barrierXone = 1;
+  double birdWidth = 0.1;
+  double birdHeight = 0.1;
+  static List<double> barrierX = [2, 2 + 1.5];
+  static double barrierWidth = 0.5;
+  List<List<double>> barrierHeight = [
+    //out of 2 , where 2 is the entire height of the screen
+    //[topHeight,bottomHeight]
+    [0.4, 0.5],
+    [0.4, 0.6],
+  ];
+  void jump() {
+    setState(() {
+      time = 0;
+      initialHeight = birdYaxis;
+    });
+  }
+
+  bool birdIsDead() {
+    if (birdYaxis < -1 || birdYaxis > 1) {
+      return true;
+    }
+
+
+    for(int i=0;i<barrierX.length;i++){
+      if(barrierX[i]<=birdWidth&&
+          barrierX[i]+barrierWidth>=-birdWidth&&
+          (birdYaxis<=-1+barrierHeight[i][0]||
+              birdYaxis+birdWidth>=1-barrierHeight[i][1])){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 50), (timer) {
+      time += 0.04;
+      height = -4.9 * time * time + 3 * time;
+      setState(() {
+        birdYaxis = initialHeight - height;
+      });
+     if(birdIsDead()){
+       timer.cancel();
+       _showDialog();
+     }
+
+     moveMap();
+
+     time+=0.01;
+
+    });
+  }
+  void moveMap(){
+    for(int i=0; i<barrierX.length;i++){
+       //keep barriers moving
+      setState(() {
+        barrierX[i]-=0.05;
+      });
+      //if the barrier exit hte left part of the screen keep it looping
+      if(barrierX[i]<-1.5){
+        barrierX[i]+=3;
+      }
+    }
+  }
+
+  void restGame() {
+    Navigator.pop(context);
+    setState(() {
+      birdYaxis = 0;
+      gameHasStarted = false;
+      time = 0;
+      initialHeight = birdYaxis;
+    });
+  }
+
+  void _showDialog() {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.brown,
+            title: Center(
+              child: Text(
+                "G A M E  O V E R ",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            actions: [
+              GestureDetector(
+                onTap: restGame,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Container(
+                    padding: EdgeInsets.all(7),
+                    color: Colors.white,
+                    child: Text(
+                      "PLAY AGAIN",
+                      style: TextStyle(color: Colors.brown),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (gameHasStarted) {
+          jump();
+        } else {
+          startGame();
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Stack(
+                  children: [
+                    AnimatedContainer(
+                      alignment: Alignment(0, birdYaxis),
+                      color: Colors.blue,
+                      duration: Duration(milliseconds: 0),
+                      child: MyBird(birdY: birdYaxis,
+                      birdWidth: birdWidth,
+                        birdHeight: birdHeight,
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment(0, -0.3),
+                      child: gameHasStarted
+                          ? Text("")
+                          : Text(
+                              "T A P   TO   P L A Y",
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.white),
+                            ),
+                    ),
+                    AnimatedContainer(
+                      alignment: Alignment(barrierXone, 1.1),
+                      duration: Duration(milliseconds: 0),
+                      child: MyBarrier(
+                        barrierX: barrierX[0],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[0][0],
+                        isThisBottomBarrier: false,
+
+                      ),
+                    ),
+                    AnimatedContainer(
+                      alignment: Alignment(barrierXone, -1.1),
+                      duration: Duration(milliseconds: 0),
+                      child:MyBarrier(
+                        barrierX: barrierX[0],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[0][1],
+                        isThisBottomBarrier: true,
+
+                      ),
+                    ),
+                    AnimatedContainer(
+                      alignment: Alignment(barrierXtwo, 1.1),
+                      duration: Duration(milliseconds: 0),
+                      child: MyBarrier(
+                        barrierX: barrierX[1],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[1][0],
+                        isThisBottomBarrier: false,
+
+                      ),
+                    ),
+                    AnimatedContainer(
+                      alignment: Alignment(barrierXtwo, -1.1),
+                      duration: Duration(milliseconds: 0),
+                      child: MyBarrier(
+                        barrierX: barrierX[1],
+                        barrierWidth: barrierWidth,
+                        barrierHeight: barrierHeight[1][1],
+                        isThisBottomBarrier: true,
+
+                      ),
+                    )
+                  ],
+                )),
+            Container(
+              height: 15,
+              color: Colors.green,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.brown,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                   Text('C R E A T E D   BY   Q A Y S',
+                     style: TextStyle(color: Colors.white, fontSize: 20),)
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
